83ae467eb4737169368bd0c0cb9c406ec3c6198a
Lucas Loughry <lucasloughry@gmail.com>
2025-10-03 12:48:32 -0300

    hey

--- src/pages/LogWorkout.jsx
+++ src/pages/LogWorkout.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react--router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function LogWorkout() {
   const [name, setName] = useState('');
