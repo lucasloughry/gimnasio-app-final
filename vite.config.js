f51861d7ddf263a0302fd836ce1c3109ccf8574f
Lucas Loughry <lucasloughry@gmail.com>
2025-10-04 08:04:50 -0300

    mas

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,   // 👈 habilita sourcemaps en el build
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      },
+    }
   },
+  plugins: [
+    react(),
+    // Configuración del plugin de PWA
+    VitePWA({
+      registerType: 'autoUpdate',
+      // Archivos a cachear para que la app funcione sin conexión
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Gimnasio Municipal',
+        short_name: 'GymApp',
+        description: 'Aplicación de seguimiento para el Gimnasio Municipal.',
+        theme_color: '#ffffff',
+        // Íconos de la aplicación que crearemos en el siguiente paso
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
-
